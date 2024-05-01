273b025726607879f162fc12f51c40fa145c3507
serdkan <serdar.kandiran@outlook.com>
2024-05-01 21:56:01 +0300

    calismalar

---
+++ js/calisma.js
@@ -0,0 +1,13 @@
+                              
+function karenincevresi(birkenaruzunlugu){
+
+  let divinicerigi = document.getElementById("hesap").innerHTML;
+  alert(divinicerigi);
+  
+}
+
+
+karenincevresi(5);
+
+//1. ödev matematiksel işlemler kullanarak fonksiyon oluştur
+//2. ödev html + css kullanarak CARD (Kart ) oluştur 
