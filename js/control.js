7616ee6159847797df72278813c1865761af0219
serdkan <serdar.kandiran@outlook.com>
2024-05-20 21:46:34 +0300

    control

---
+++ js/control.js
@@ -0,0 +1,8 @@
+function control(){
+    let kullanici = document.getElementById("kadi").value;
+    let sifre = document.getElementById("sifre").value;
+
+    if(kullanici=="serdar" && sifre=="123456"){
+        window.location.href = "calisma1.html";
+    }
+}
