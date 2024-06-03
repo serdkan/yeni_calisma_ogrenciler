e59ca4444d461db04ba90eaa5d9bf7f6d297dc65
serdkan <serdar.kandiran@outlook.com>
2024-06-03 22:00:12 +0300

    kartlar

--- js/calisma6.js
+++ js/calisma6.js
@@ -1,11 +1,36 @@
-//object
-//
+
 let urunler = [
-  { adi: "apple watch", fiyat: "2500 TL", resim: "resimler/apple.jpg" },
-  { adi: "samsung telefon", fiyat: "10000 TL" },
-  { adi: "playstation 5", fiyat: "23 000 TL" },
+  {
+    urunresim: "resimler/kolsaat.jpg",
+    urunadi: "Akıllı Saat TCL",
+    puanlama: "resimler/yildizlar.png",
+    fiyat: "299 $",
+    urunaciklamasi:"kol saati çocuk için GPS özellikli",
+  },
+  {
+    urunresim: "/resimler/iphone.png",
+    urunadi: "i phone 14 pro",
+    puanlama: "resimler/yildizlar.png",
+    fiyat: "500 $",
+    urunaciklamasi:"iphone taze yeni geldi"
+  },
+  {
+    urunresim: "/resimler/telvizyon.png",
+    urunadi: "LG televizyon",
+    puanlama: "resimler/yildizlar.png",
+    fiyat: "320 $",
+    urunaciklamasi:"led ekran LG 0 televizyon"
+  },
+  {
+    urunresim: "/resimler/ipad.png",
+    urunadi: "ipad",
+    puanlama: "resimler/yildizlar.png",
+    fiyat: "180 $",
+    urunaciklamasi:"kitab okumak için ipad"
+  },
 ];
-console.log(urunler[0].adi);
-console.log(urunler[1].fiyat);
-console.log(urunler[2].adi);
-console.log(urunler[0].resim);
+
+document.getElementById("urunresmi").innerHTML = `<img width="200px" src="${urunler[0].urunresim}" alt="">`;
+document.getElementById("fiyat").innerHTML = `${urunler[0].fiyat}`;
+document.getElementById("urunadi").innerHTML = `${urunler[0].urunadi.toUpperCase()}`;
+document.getElementById("aciklama").innerHTML = `${urunler[0].urunaciklamasi}`;
