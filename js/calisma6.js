43a7c947152e4d5a503999040962f94b37cbbe74
serdkan <serdar.kandiran@outlook.com>
2024-06-05 21:58:43 +0300

    kart çalışmasi

--- js/calisma6.js
+++ js/calisma6.js
@@ -1,36 +1,13 @@
-
-let urunler = [
+let dersler = [
   {
-    urunresim: "resimler/kolsaat.jpg",
-    urunadi: "Akıllı Saat TCL",
-    puanlama: "resimler/yildizlar.png",
-    fiyat: "299 $",
-    urunaciklamasi:"kol saati çocuk için GPS özellikli",
-  },
-  {
-    urunresim: "/resimler/iphone.png",
-    urunadi: "i phone 14 pro",
-    puanlama: "resimler/yildizlar.png",
-    fiyat: "500 $",
-    urunaciklamasi:"iphone taze yeni geldi"
-  },
-  {
-    urunresim: "/resimler/telvizyon.png",
-    urunadi: "LG televizyon",
-    puanlama: "resimler/yildizlar.png",
-    fiyat: "320 $",
-    urunaciklamasi:"led ekran LG 0 televizyon"
-  },
-  {
-    urunresim: "/resimler/ipad.png",
-    urunadi: "ipad",
-    puanlama: "resimler/yildizlar.png",
-    fiyat: "180 $",
-    urunaciklamasi:"kitab okumak için ipad"
+    derskategori: "FRONTEND",
+    puan: "4.9",
+    izlenmeorani: "1.25K",
+    baslik:"Beginner Javascript",
   },
 ];
 
-document.getElementById("urunresmi").innerHTML = `<img width="200px" src="${urunler[0].urunresim}" alt="">`;
-document.getElementById("fiyat").innerHTML = `${urunler[0].fiyat}`;
-document.getElementById("urunadi").innerHTML = `${urunler[0].urunadi.toUpperCase()}`;
-document.getElementById("aciklama").innerHTML = `${urunler[0].urunaciklamasi}`;
+document.getElementById("web").innerHTML = dersler[0].derskategori;
+document.getElementById("puan").innerHTML = dersler[0].puan;
+document.getElementById("izlenmeorani").innerHTML =  " &nbsp;&nbsp;&nbsp;&nbsp;" + dersler[0].izlenmeorani;
+document.getElementById("baslik").innerHTML = dersler[0].baslik;
